11ceb981023529c2312e978a33f4979ab420f614
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-20 23:39:13 +0100

    chore: Bump Elixir version
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule M51.MixProject do
     [
       app: :matrix2051,
       version: version(),
-      elixir: "~> 1.7",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [
